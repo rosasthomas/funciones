a30b7bc23fd74a0897354aad5dd67a55365a3eb4
rosasthomas <rosasthomas2@gmail.com>
2018-08-28 11:33:36 -0300

    funciones

--- funciones/main.c
+++ funciones/main.c
@@ -16,47 +16,13 @@ int main()
     scanf("%d", &numeroUno);
     printf("Ingrese el segundo numero: ");
     scanf("%d", &numeroDos);
+
     suma = sumar4(numeroUno, numeroDos);
+
     printf("La suma es: %d", suma);
+
     return 0;
 }
-
-int sumar3(void)
-{
-    int numeroUno;
-    int numeroDos;
-    int suma;
-
-    printf("Ingrese el primer numero: ");
-    scanf("%d", &numeroUno);
-    printf("Ingrese el segundo numero: ");
-    scanf("%d", &numeroDos);
-
-    suma = numeroUno + numeroDos;
-    //sumar1();
-    return suma;
-}
-
-
-void sumar2(int unNumero, int otroNumero)
-{
-    int suma;
-
-    suma = unNumero + otroNumero;
-    printf("El resultado es: %d", suma);
-}
-
-int sumar4 (int numeroUno, int numeroDos)
-{
-    int suma;
-    suma = numeroUno + numeroDos;
-
-
-    return suma;
-}
-
-
-
 
 void sumar1(void)
 {
@@ -72,4 +38,36 @@ void sumar1(void)
     suma = numeroUno + numeroDos;
 
     printf("El resultado es: %d", suma);
+}
+
+void sumar2(int unNumero, int otroNumero)
+{
+    int suma;
+
+    suma = unNumero + otroNumero;
+    printf("El resultado es: %d", suma);
+}
+
+int sumar3(void)
+{
+    int numeroUno;
+    int numeroDos;
+    int suma;
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &numeroUno);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &numeroDos);
+
+    suma = numeroUno + numeroDos;
+
+    return suma;
+}
+
+int sumar4 (int numeroUno, int numeroDos)
+{
+    int suma;
+    suma = numeroUno + numeroDos;
+
+    return suma;
 }
