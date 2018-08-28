8cdc5e4eb8e535908afa99ee126b955d38803ca5
rosasthomas <rosasthomas2@gmail.com>
2018-08-28 11:30:46 -0300

    funciones sumar4

--- funciones/main.c
+++ funciones/main.c
@@ -4,12 +4,20 @@
 void sumar1(void);
 void sumar2 (int, int);
 int sumar3(void);
+int sumar4 (int, int);
 
 int main()
 {
+    int numeroUno;
+    int numeroDos;
     int suma;
-    suma = sumar3();
-    printf("La suma es : %d", suma);
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &numeroUno);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &numeroDos);
+    suma = sumar4(numeroUno, numeroDos);
+    printf("La suma es: %d", suma);
     return 0;
 }
 
@@ -38,7 +46,14 @@ void sumar2(int unNumero, int otroNumero)
     printf("El resultado es: %d", suma);
 }
 
+int sumar4 (int numeroUno, int numeroDos)
+{
+    int suma;
+    suma = numeroUno + numeroDos;
 
+
+    return suma;
+}
 
 
 
