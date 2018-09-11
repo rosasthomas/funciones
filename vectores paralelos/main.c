6525a8f96bd4db1a59c13f9c9d6bfdecf9a60402
rosasthomasgit config --global user.name rosasthomasgit config --global user.name rosasthomas <rosasthomas2@gmail.com>
2018-09-11 12:17:59 -0300

    tarea

--- vectores paralelos/main.c
+++ vectores paralelos/main.c
@@ -2,12 +2,30 @@
 #include <stdlib.h>
 #define T 3
 
+void cargarAlumnos(int[], char[][21], int[], float[],int);
+void mostrarAlumnos(int[], char[][21], int[], float[],int);
+/*
+-ordenar por nombre;
+-mostrar los alumnos aprobrados;
+-mostrar los alumnos que se llaman juan;
+-mostrar los alumnos cuyo nombre comience con P;
+*/
 int main()
 {
     int legajos[T];
     char nombres[T][21];
     int notas[T];
     float alturas[T];
+    char mail[T][50];
+
+    cargarAlumnos(legajos, nombres, notas, alturas, T);
+    mostrarAlumnos(legajos, nombres, notas, alturas, T);
+
+    return 0;
+}
+
+void cargarAlumnos(int legajos[], char nombres[][21], int notas[], float altura[],int T)
+{
     int i;
 
     for(i = 0; i < T; i++)
@@ -25,12 +43,13 @@ int main()
         printf("Ingrese altura: ");
         scanf("%f", &alturas[i]);
     }
+}
 
+void mostrarAlumnos(int legajos[], char nombres[][21], int notas[], float altura[],int T)
+{
     printf("%4s %5s %2s %5s \n", "Legajo", "Nombre", "Nota", "Altura");
     for(i = 0; i < T; i++)
     {
         printf("%4d  %5s  %2d  %5.2f\n", legajos[i], nombres[i], notas[i], alturas[i]);
     }
-
-    return 0;
 }
